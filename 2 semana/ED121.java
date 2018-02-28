bfb9a4cec41d53131a56bcd1c1bf10d5d02e12d2
XxArcaiCxX <peuporsche@hotmail.com>
2018-02-28 10:56:39 +0000

    Update ED121.java

--- 2 semana/ED121.java
+++ 2 semana/ED121.java
@@ -5,19 +5,25 @@ public class ED121{
   public static void main(String [] args){
     Scanner stdin = new Scanner(System.in);
     int linhas = stdin.nextInt();
+    int l = 0;
+    int k = 0;
+    String lixo = stdin.nextLine();
+    System.out.println(linhas);
     for(int i=0; i<linhas; i++){
       String linha = stdin.nextLine();
       linha = linha.replaceAll("\\s+","");
       int letras = linha.length();
-      if(letras % 2 == 0)
-        int l = letras/2;       //par
-      else
-        int l = letras/2 + 1;   //impar
-      boolean igual = new boolean;
+        l = letras/2;
+      int flag = 0;
       for(int j=0; j<l; j++){
-        int k = l + j;
+        k = letras - 1 - j;
+        if(linha.charAt(j) != linha.charAt(k))
+          flag = 1;
       }
+      if(flag == 0)
+        System.out.println("Sim");
+      else if(flag == 1)
+        System.out.println("Nao");
     }
   }
-
 }
